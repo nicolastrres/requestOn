3f71d401ce558e7381a0dd0e9112c400cec03a90
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-01-22 10:05:24 -0300

    Adding parse_args function to the main file

--- checkit/__main__.py
+++ checkit/__main__.py
@@ -1,1 +1,6 @@
 import argparse
+
+def parse_args(req_args=None):
+    parser = argparse.ArgumentParser(prog="Checkit")
+    group_request = parser.add_argument_group("Request options")
+    group_request.add_argument("-r", "--request", action="store", type="string", destination="request_url")
