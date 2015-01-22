0ed3a71dbb61a02c2221a7014cc27b96706b09b8
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-01-22 09:55:09 -0300

    Changing the project structure

--- tests/test.py
+++ tests/test.py
@@ -3,13 +3,11 @@ import sys
 import os.path
 import inspect
 
-# sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-__file__ = os.path.abspath(inspect.getsourcefile(lambda _: None))
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-sys.path.insert(0, ROOT_DIR)
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from CHECKIT.request_service import RequestService
+
+
 
 class RequestServiceTest(unittest.TestCase):
     def setUp(self):
