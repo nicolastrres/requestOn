893644f239c8d9d4c451230e8dd0885e14dd72f6
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-01-22 10:10:06 -0300

    Creating the main function in the __main__.py file

--- checkit/__main__.py
+++ checkit/__main__.py
@@ -4,3 +4,10 @@ def parse_args(req_args=None):
     parser = argparse.ArgumentParser(prog="Checkit")
     group_request = parser.add_argument_group("Request options")
     group_request.add_argument("-r", "--request", action="store", type="string", destination="request_url")
+
+    args = parser.parse_args(req_args)
+    return args
+
+
+def main(req_args=None):
+    args = parse_args(req_args)
