4ef60de2a996ccb8a9e81ca435130011b41ceb87
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-02-05 23:42:20 -0300

    Fixing pep8 warnings

--- checkit/logs.py
+++ checkit/logs.py
@@ -1,13 +1,14 @@
 import logging
 
+
 class Logs():
     def log(self):
-        logging.basicConfig(filename='example.log',level=logging.DEBUG)
+        logging.basicConfig(filename='example.log', level=logging.DEBUG)
         logging.debug('This message should go to the log file')
         logging.info('So should this')
         logging.warning('And this, too')
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     logs = Logs()
     logs.log()
