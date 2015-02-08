c37eeb12b164c8e6c4140ff37d2c75fcc7edcdcf
roselmamendes <roselma.mendes@gmail.com>
2015-02-08 23:17:39 +0000

    Starting development for logs in the project.

--- checkit/logs.py
+++ checkit/logs.py
@@ -2,13 +2,16 @@ import logging
 
 
 class Logs():
-    def log(self):
-        logging.basicConfig(filename='example.log', level=logging.DEBUG)
-        logging.debug('This message should go to the log file')
-        logging.info('So should this')
-        logging.warning('And this, too')
-
-
-if __name__ == "__main__":
-    logs = Logs()
-    logs.log()
+    logging.basicConfig(level=logging.INFO)
+        
+    def info(message):
+        logging.info("Calling endpoint: " + message)
+        
+    def error_status_code(status_code):
+        logging.error("Error %s" % status_code)
+        
+    def general_error(message):
+        logging.error("Undefined Error: " + message)
+        
+    def extra_info(message):
+        logging.info("Extra information: " + message)
--- tests/test.py
+++ tests/test.py
@@ -5,8 +5,7 @@ import inspect
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from CHECKIT.request_service import RequestService
-
+from checkit.request_service import RequestService
 
 
 class RequestServiceTest(unittest.TestCase):
@@ -20,6 +19,10 @@ class RequestServiceTest(unittest.TestCase):
     def test_not_found(self):
         response = self.request.getcode('http://www.github.com/sdksdjflskjflskjflsdkjflskdjfsfds')
         self.assertEqual(response, 404)
+        
+    def test_invalid_url(self):
+        response = self.request.getcode('test')
+        self.assertEqual(response[0], "unknown url type: 'test'")
 
 if __name__ == '__main__':
     unittest.main()
