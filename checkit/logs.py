a847ca99571e196e9e4366a9e7dc3269ac005a4f
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-02-09 22:21:49 -0300

    Added @staticmethod because we are calling the methods without create an object

--- checkit/logs.py
+++ checkit/logs.py
@@ -3,15 +3,19 @@ import logging
 
 class Logs():
     logging.basicConfig(level=logging.INFO)
-        
+
+    @staticmethod
     def info(message):
         logging.info("Calling endpoint: " + message)
-        
+
+    @staticmethod
     def error_status_code(status_code):
         logging.error("Error %s" % status_code)
-        
+
+    @staticmethod
     def general_error(message):
         logging.error("Undefined Error: " + message)
-        
+
+    @staticmethod
     def extra_info(message):
         logging.info("Extra information: " + message)
