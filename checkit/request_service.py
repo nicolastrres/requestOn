d4a68865af2ee1747b06730a7f1627084b7a2623
Roselma Mendes <rmendes@thoughtworks.com>
2015-02-22 00:15:53 -0300

    Removed get_code method from request_service.

--- checkit/request_service.py
+++ checkit/request_service.py
@@ -16,21 +16,6 @@ class RequestService():
         self.api_name = api_name
         self.endpoints = []
 
-    def get_code(self, url):
-        try:
-            code = urllib.request.urlopen(url).getcode()
-            Logs.info(url + " --- code response:" + str(code))
-            return code
-        except urllib.error.HTTPError as e:
-            Logs.error_status_code(e.code)
-            return e.code
-        except urllib.error.URLError as e:
-            Logs.general_error(e.reason)
-            return e.reason
-        except ValueError as e:
-            Logs.general_error("".join(e.args))
-            return e.args
-
     def addEndpoint(self, endpoint):
         self.endpoints.append(endpoint)
 
