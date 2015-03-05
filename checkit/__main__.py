be9d52123e2621d657ea337214876786895a80e3
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-03-04 22:29:24 -0300

    Adding usage function

--- checkit/__main__.py
+++ checkit/__main__.py
@@ -1,12 +1,21 @@
 import argparse
+import sys
 from request_service import RequestService
 from logs import Logs
 
+def usage():
+    print("\t\t\t Checkit\n\n")
+    print("Usage: checkit -r target [-l log_file_name]")
+    print("-t --target                  - target to be requested. Required")
+    print("-l --log                     - name of the file where the logs are going to be saved. Optional")
+    print("\n\nExamples:\n checkit -t http://www.google.com")
+    print(" checkit -t http://www.facebook.com -l logs.txt")
+    sys.exit(0)
 
 def parse_args():
     parser = argparse.ArgumentParser(prog="Checkit")
     group_request = parser.add_argument_group("Request options")
-    group_request.add_argument("-r", "--request", action="store", dest="request_url",
+    group_request.add_argument("-t", "--target", action="store", dest="target_url",
                                help="Do a request to an specific url")
     group_log = parser.add_argument_group("Logs options")
     group_log.add_argument("-l", "--log", action="store", dest="log_file_name",
@@ -17,14 +26,17 @@ def parse_args():
 
 
 def main():
-    requestService = RequestService()
+    if not len(sys.argv[1:]):
+        usage()
     args = parse_args()
+
     if args.log_file_name:
         logs.write_file(filename=args.log_file_name)
-    if args.request_url:
-        requestService.get_code(url=args.request_url, logs=logs)
+    if args.target_url:
+        requestService.get_code(url=args.target_url, logs=logs)
 
 
 if __name__ == "__main__":
+    requestService = RequestService()
     logs = Logs()
     main()
