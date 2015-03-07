35656dfc838671aaa665cf523120303bdf9b714e
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-03-07 08:15:53 -0300

    Trying to break the build when pep8 fails

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+-e
 echo '---------------------------Running PEP8---------------------------'
 for f in $(find requestOn -name '*.py' -or -name '*.doc'); do pep8 $f; done
 echo '------------------------------------------------------------------'
